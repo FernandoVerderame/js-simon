99b631014cc1f01eeb5730b8fdc21cceb1ffee3a
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-01-12 20:32:52 +0100

    print on page five random numbers

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ const numbers = document.getElementById('random-numbers');
 
 // Array di 5 numeri casuali
 const randomNumbers = [];
+
+
+// Template Literal
+let listItems = '<ul class="five-numbers">';
+
+// Genero 5 numeri casuali da visualizzare in pagina
+for (let i = 0; i < 5; i++) {
+    const generateRandomNumbers = Math.floor(Math.random() * 99) + 1;
+    randomNumbers.push(generateRandomNumbers);
+    listItems += `<li>${randomNumbers[i]}</li>`;
+}
+
+listItems += '</ul>';
+
+// Inserisco in pagina i 5 numeri casuali
+numbers.innerHTML = listItems;
