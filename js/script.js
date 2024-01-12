ff1d259886cb25af7dd42c4d2a68aab94c380bb3
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-01-12 20:36:24 +0100

    created countdown function

--- js/script.js
+++ js/script.js
@@ -26,3 +26,21 @@ numbers.innerHTML = listItems;
 // Inizializzo i secondi
 let seconds = 5;
 timer.innerText = seconds;
+
+// ! FUNZIONE COUNTDOWN
+const countdown = setInterval(() => {
+
+    // Decremento il timer
+    timer.innerText = --seconds;
+
+    // Stoppo il timer allo 0
+    if (seconds === 0) {
+        timer.innerText = '0';
+
+        // Faccio sparire i numeri random
+        numbers.classList.add('d-none');
+        clearInterval(countdown);
+
+    }     
+    
+}, 1000);
