691d3c1c9e8e9adf9444ed31bf0faa4c1d34f63b
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-01-12 20:25:53 +0100

    added random numbers array

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@
 // Recuperiamo gli elementi
 const timer = document.getElementById('timer');
 const numbers = document.getElementById('random-numbers');
+
+// Array di 5 numeri casuali
+const randomNumbers = [];
